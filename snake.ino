fa098b4b245713a6c76a2fb8a4b325628db357d3
Malcolm Mielle <malcolm.mielle@protonmail.com>
2017-05-04 01:05:47 +0200

    better wave + sonar for view

--- snake.ino
+++ snake.ino
@@ -4,55 +4,148 @@
 #include "snakeposition.h"
 #include "grad.h"
 
-Servo head;  // create servo object to control a servo
+#include <NewPing.h>
+
+#define TRIGGER_PIN  26  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     10  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 40 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
+Servo vservo1;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
-Servo neck;
-Servo body;
-Servo tail;
+Servo hservo2;
+Servo vservo3;
+Servo hservo4;
+Servo vservo5;
+Servo hservo6;
+Servo vservo7;
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 
 int pos = 0;    // variable to store the servo position
 
+int curve(int pos, int shift, int max_in, int min_in){
+  int pos_servo2 = pos + shift;
+  while(pos_servo2 > max_in || pos_servo2 < min_in){
+    
+    if(pos_servo2 < min_in){
+      pos_servo2 = min_in + (min_in - pos_servo2);
+    }
+    if(pos_servo2 > max_in){
+      pos_servo2 = max_in - (pos_servo2 - max_in);
+    }
 
-int curve_up(int pos, int shift, int max){
-  int pos_neck = pos + shift;
-  if(pos_neck > max){
-    pos_neck = max - (pos_neck - max);
   }
-  return pos_neck;
+}
+
+int curve_up(int pos, int shift, int max_in){
+  int pos_servo2 = pos + shift;
+  if(pos_servo2 > max_in){
+    pos_servo2 = max_in - (pos_servo2 - max_in);
+  }
+  else{
+    Serial.println(" nopup ");
+    Serial.println(pos_servo2);
+    Serial.println(max_in);
+    Serial.println(pos);
+    Serial.println(shift);
+  }
+  return pos_servo2;
 }
 
 int curve_down(int pos, int shift, int min){
-  int pos_neck = pos - shift;
-  if(pos_neck < min){
-    pos_neck = min + (min - pos_neck);
+  int pos_servo2 = pos - shift;
+  if(pos_servo2 < min){
+    pos_servo2 = min + (min - pos_servo2);
   }
-  return pos_neck;
+  else{
+    Serial.println(" nop ");
+    Serial.println(pos_servo2);
+    Serial.println(min);
+  }
+  return pos_servo2;
 }
 
 
-void move(int pos, int pos_neck, int pos_body, int pos_tail){
-  head.write(pos);              // tell servo to go to position in variable 'pos'
-  neck.write(pos_neck);              // tell servo to go to position in variable 'pos'
-  body.write(pos_body);
-  tail.write(pos_tail);
-  delay(15);
+void move(int pos, int pos_servo2, int pos_vservo3, int pos_servo4){
+  vservo1.write(pos);              // tell servo to go to position in variable 'pos'
+  hservo2.write(pos_servo2);              // tell servo to go to position in variable 'pos'
+  vservo3.write(pos_vservo3);
+  hservo4.write(pos_servo4);
+  // delay(5);
+}
+
+void moveFull(int pos, int pos_servo2, int pos_vservo3, int pos_servo4, int pos_vservo5, int pos_hservo6, int pos_vservo7){
+  vservo1.write(pos);              // tell servo to go to position in variable 'pos'
+  hservo2.write(pos_servo2);              // tell servo to go to position in variable 'pos'
+  vservo3.write(pos_vservo3);
+  hservo4.write(pos_servo4);
+  vservo5.write(pos_vservo5);
+  hservo6.write(pos_hservo6);
+  vservo7.write(pos_vservo7);
+  delay(10);
 }
 
 void curver(){
   for (pos = 40; pos <= 140; pos += 1) { // goes from 0 degrees to 180 degrees
-    int pos_neck = curve_up(pos, 20, 140);
-    int pos_body = curve_up(pos, 40, 140);
+    int pos_servo2 = curve_up(pos, 20, 140);
+    int pos_vservo3 = curve_up(pos, 40, 140);
 
     // in steps of 1 degree
-    move(0, pos_neck, 90, pos_body);
+    move(0, pos_servo2, 90, pos_vservo3);
   }
   for (pos = 140; pos >= 40; pos -= 1) { // goes from 180 degrees to 0 degrees
-    int pos_neck = curve_down(pos, 20, 40);
-    int pos_body = curve_down(pos, 40, 40);
+    int pos_servo2 = curve_down(pos, 20, 40);
+    int pos_vservo3 = curve_down(pos, 40, 40);
     
-    move(0, pos_neck, 90, pos_body);
+    move(0, pos_servo2, 90, pos_vservo3);
   }
+}
+
+void curverFull(int shift, int max_in, int min_in){
+  int pos = 0;
+  int pos_vservo1 = -1;
+  do{
+    pos_vservo1 = pos;
+    pos = pos + 1;
+    int pos_vservo1 = curve(pos,   shift, max_in, min_in);
+    int pos_vservo3 = curve(pos, 2*shift, max_in, min_in);
+    int pos_vservo5 = curve(pos, 3*shift, max_in, min_in);
+    int pos_vservo7 = curve(pos, 4*shift, max_in, min_in);
+
+    // Serial.println("Pisition");
+    // Serial.println(pos);
+    // Serial.println(pos_vservo1);
+    
+    moveFull(pos_vservo1, 90, pos_vservo3, 90, pos_vservo5, 90, pos_vservo7);
+    // Serial.println(pos_vservo1);
+  } while (pos < (max_in - min_in) * 2);
+    Serial.println("out");
+  // for (pos = min_in; pos <= max_in; pos += 1) { // goes from 0 degrees to 180 degrees
+  //   int pos_vservo1 = curve(pos,   shift, max_in, min_in);
+  //   int pos_vservo3 = curve(pos, 2*shift, max_in, min_in);
+  //   int pos_vservo5 = curve(pos, 3*shift, max_in, min_in);
+  //   int pos_vservo7 = curve(pos, 4*shift, max_in, min_in);
+
+  //   Serial.println("Pisition");
+  //   // Serial.println(pos_vservo1);
+  //   // Serial.println(pos_vservo3);
+  //   // Serial.println(pos_vservo5);
+  //   Serial.println(pos_vservo7);
+
+  //   // in steps of 1 degree
+  //   moveFull(pos_vservo1, 90, pos_vservo3, 90, pos_vservo5, 90, pos_vservo7);
+  // }
+  // for (pos = max_in; pos >= min_in; pos -= 1) { // goes from 180 degrees to 0 degrees
+  //   int pos_vservo1 = curve(pos,   shift, max_in, min_in);
+  //   int pos_vservo3 = curve(pos, 2*shift, max_in, min_in);
+  //   int pos_vservo5 = curve(pos, 3*shift, max_in, min_in);
+  //   int pos_vservo7 = curve(pos, 4*shift, max_in, min_in);
+  //   Serial.println(pos_vservo7);
+  
+  //   moveFull(pos_vservo1, 90, pos_vservo3, 90, pos_vservo5, 90, pos_vservo7);
+  // }
 }
 
 
@@ -91,10 +184,13 @@ void testKine(){
 
 
 void setup() {
-  head.attach(2);  // attaches the servo on pin 9 to the servo object
-  neck.attach(3);
-  body.attach(4);
-  tail.attach(5);
+  vservo1.attach(2);  // attaches the servo on pin 9 to the servo object
+  hservo2.attach(3);
+  vservo3.attach(4);
+  hservo4.attach(5);
+  vservo5.attach(6);
+  hservo6.attach(7);
+  vservo7.attach(8);
   Serial.begin(9600);
 }
 
@@ -111,8 +207,8 @@ float angleToServo(float a){
     }
   }
 
-  Serial.println("a");
-  Serial.println(a);
+  // Serial.println("a");
+  // Serial.println(a);
 
   double serv = a * 180 / pi;
   return serv;
@@ -128,11 +224,11 @@ void moveToPose(float x_in, float y_in, float z_in){
   float tcserv = angleToServo(tc); 
   float tfserv = angleToServo(tf);
 
-  Serial.println("serv ");
-  Serial.println(taserv);
-  Serial.println(tbserv);
-  Serial.println(tcserv);
-  Serial.println(tfserv);
+  // Serial.println("serv ");
+  // Serial.println(taserv);
+  // Serial.println(tbserv);
+  // Serial.println(tcserv);
+  // Serial.println(tfserv);
   // have we gone past it?
   
   move(tfserv, tcserv, tbserv, taserv);  
@@ -150,8 +246,14 @@ void loop() {
   // delay(1000);
   // moveToPose(150, -100, 0);
   // delay(1000);
-
-curver();
+// curver();
+int dist = sonar.ping_cm();
+Serial.print("dist ");
+Serial.println(dist);
+if(dist < 20 && dist > 10){
+  curverFull(40, 120, 20);
+}
+// moveFull(90, 90, 90, 90, 90, 90, 90);
 
 
   // delay(1000);
